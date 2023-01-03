67fb4e625d128d82dcb9086cce1e95d14d01fb9c
Norihiko Suehiro <coooooooolasacucumber@gmail.com>
2023-01-03 15:46:17 +0900

    🔥 Remove file

--- client/src/utils/constants.js
+++
@@ -1,5 +0,0 @@
-import abi from "./Transactions.json";
-
-export const contractABI = abi.abi;
-// deploy.jsでデプロイした際に取得するアドレス
-export const contractAddress = "0x54A1E0C1422B4C34cA0b26A0d251071741750244";
